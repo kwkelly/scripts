6124784ffc59429b6af5eaae7a58d3169efe713a
Keith Kelly <keith@ices.utexas.edu>
2015-10-18 22:38:05 -0500

    export for password store

---
+++ pass_export.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# export passwords to external file
+
+prefix=${PASSWORD_STORE_DIR:-$HOME/.password-store}
+
+for file in "$prefix"/*.gpg; do                           
+	file="${file/$prefix//}"
+	printf "%s\n" "Name: ${file%.*}" >> exported_passes
+	pass "${file%.*}" >> exported_passes
+	printf "\n\n" >> exported_passes
+done
